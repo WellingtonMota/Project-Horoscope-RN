59c4dd4802d1f5e075451136020421354cfd757b
Wellington Silva da Mota <silvaemota@gmail.com>
2018-10-17 11:00:29 -0300

    Criado projeto horoscopo

---
+++ App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+type Props = {};
+
+export default class App extends Component<Props> {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Horoscope!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
