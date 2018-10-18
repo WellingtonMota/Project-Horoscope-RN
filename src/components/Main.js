b8968b52093fa7b245f9bd6f05f7b09a28558955
Wellington Silva da Mota <silvaemota@gmail.com>
2018-10-18 11:48:43 -0300

    Melhorado estilos da página principal

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,22 +2,50 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 export default class Main extends Component {
   render() {
     return (
-      <View style={styles.main}>
-        <Text>Página Principal</Text>
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.text}>TEXTO</Text>
+        </View>
+        <View style={styles.footer}>
+          <TouchableHighlight
+            onPress={() => { Actions.signs(); }}
+          >
+            <Text style={styles.menu}>...</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
+    backgroundColor: '#00a8b9',
     flex: 1,
-    backgroundColor: '#61BD8C'
+    flexDirection: 'column'
+  },
+  section: {
+    flex: 7
+  },
+  footer: {
+    backgroundColor: '#ffffff',
+    flex: 1
+  },
+  menu: {
+    color: '#00a8b9',
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  text: {
+    color: '#00a8b9',
+    fontSize: 18
   }
 });
