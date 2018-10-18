f1581cd276e297a06d3a3cef7b75b4e3f1c6ba70
Wellington Silva da Mota <silvaemota@gmail.com>
2018-10-18 11:50:03 -0300

    Melhorado estilos de rotas emenu

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,43 @@
 import React from 'react';
-
+import {
+  StyleSheet
+} from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
 import Main from './components/Main';
+import Signs from './components/Signs';
 
 const Routes = () => (
-  <Router sceneStyle={{ paddingTop: 50 }}>
+  <Router 
+    navigationBarStyle={styles.navBar} 
+    titleStyle={styles.navBarTitle} 
+    barButtonTextStyle={styles.barButtonTextStyle} 
+    barButtonIconStyle={styles.barButtonIconStyle}
+  >
     <Scene key='root'>
-      <Scene key='Main' component={Main} initil title='Página principal' />
+      <Scene key='main' component={Main} initial title='Horóscopo do dia' />
+      <Scene key='signs' component={Signs} title='Escolha um signo' />
     </Scene>
   </Router>
 );
+
+const styles = StyleSheet.create({
+  navBar: {
+    backgroundColor: '#00a8b9',
+    height: 40
+  },
+  navBarTitle: {
+    color: '#FFFFFF',
+    flex: 1,
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  barButtonTextStyle: {
+    color: '#FFFFFF'
+  },
+  barButtonIconStyle: {
+    tintColor: '#FFFFFF'
+  }
+});
 
 export default Routes;
