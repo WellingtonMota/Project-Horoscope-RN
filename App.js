2dfaa6ce3deca7c426ddfbc131b3cecacb44ed37
Wellington Silva da Mota <silvaemota@gmail.com>
2018-10-24 18:33:46 -0300

    Incluído Firebase

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import firebase from '@firebase/app';
 import Routes from './src/Routes';
 
 export default class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAWcFak1T0pveEv6BG8t84_W4JjkKKp-4s',
+      authDomain: 'horoscope-61a6b.firebaseapp.com',
+      databaseURL: 'https://horoscope-61a6b.firebaseio.com',
+      projectId: 'horoscope-61a6b',
+      storageBucket: 'horoscope-61a6b.appspot.com',
+      messagingSenderId: '1098170718093'
+    };
+    firebase.initializeApp(config);
+  }
+
   componentDidMount() {
     SplashScreen.hide();
   }
